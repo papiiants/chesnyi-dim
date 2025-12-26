cab3089a45c9fdaa3de83dfb8f71f6a6e690ff21
Dmytro Papiiants <papiyants@gmail.com>
2025-12-26 17:29:09 +0100

    fix: getPage

--- src/lib/getPage.ts
+++ src/lib/getPage.ts
@@ -5,10 +5,9 @@ export default async function getPage(slug: string): Promise<WPPage | null> {
   if (!slug) return null
 
   try {
-    const data = await fetchAPI<WPPage[]>(
-      `/wp/v2/pages?slug=${slug}&_fields=id,slug,title,content,acf,parent&_embed`,
-      { next: { revalidate: 60 } }
-    )
+    const data = await fetchAPI<WPPage[]>(`/wp/v2/pages?slug=${slug}&_embed`, {
+      next: { revalidate: 60 }
+    })
 
     return data?.[0] ?? null
   } catch (err: any) {
