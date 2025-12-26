9f68f1f05629897b42d9da8f980abee793f8c482
Dmytro Papiiants <papiyants@gmail.com>
2025-12-26 17:39:51 +0100

    fix: getPage again

--- src/lib/getPage.ts
+++ src/lib/getPage.ts
@@ -13,6 +13,6 @@ export default async function getPage(slug: string): Promise<WPPage | null> {
   } catch (err: any) {
     console.error(`✗ Ошибка загрузки страницы "${slug}":`, err.message)
     if (err.cause) console.error('Причина:', err.cause)
-    return null // ← это спасает сборку!
+    return null
   }
 }
